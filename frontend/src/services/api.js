f80e6e47534eb879cc49937296dee421d166fcd2
nielmelekh <nielyesh1@gmail.com>
2026-06-28 23:38:11 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 // src/services/api.js
-const BASE_URL = process.env.BACKEND_URL; // Target backend server 
+const BASE_URL = process.env.REACT_APP_BACKEND_URL; // Target backend server 
 
 export const loginCall = (credentials) => {
     return fetch(`${BASE_URL}/api/auth/login`, {
