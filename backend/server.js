b97a260b335f784ab22ffe4fa602fa081984f11b
nielmelekh <nielyesh1@gmail.com>
2026-06-28 23:56:06 +0300

    adapted sockets for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // Initializing Socket.io and attaching it to the server with its own CORS policy
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.FRONTEND_URL,
         methods: ["GET", "POST"]
     }
 });
